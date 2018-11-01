dc1e32caf3ec74205f1cd3f996abc8151983ed8a
YinQ <yinquan@jahosp.com>
2018-11-01 15:07:08 +0800

    Initial commit

---
+++ Example/CherryRequest/CRViewController.m
@@ -0,0 +1,29 @@
+//
+//  CRViewController.m
+//  CherryRequest
+//
+//  Created by richardyq@163.com on 11/01/2018.
+//  Copyright (c) 2018 richardyq@163.com. All rights reserved.
+//
+
+#import "CRViewController.h"
+
+@interface CRViewController ()
+
+@end
+
+@implementation CRViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
