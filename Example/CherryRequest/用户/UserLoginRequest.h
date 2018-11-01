3362f1efc9bd775ba507cb3cf18dee7dc15f8c88
YinQ <yinquan@jahosp.com>
2018-11-01 16:55:42 +0800

    YinQ

---
+++ Example/CherryRequest/用户/UserLoginRequest.h
@@ -0,0 +1,14 @@
+//
+//  UserLoginRequest.h
+//  CherryRequest_Example
+//
+//  Created by YinQ on 2018/11/1.
+//  Copyright © 2018年 richardyq@163.com. All rights reserved.
+//
+
+
+#import "CDJsonRequest.h"
+
+@interface UserLoginRequest : CDJsonRequest
+
+@end
---
+++ Example/CherryRequest/用户/UserLoginRequest.m
@@ -0,0 +1,13 @@
+//
+//  UserLoginRequest.m
+//  CherryRequest_Example
+//
+//  Created by YinQ on 2018/11/1.
+//  Copyright © 2018年 richardyq@163.com. All rights reserved.
+//
+
+#import "UserLoginRequest.h"
+
+@implementation UserLoginRequest
+
+@end
